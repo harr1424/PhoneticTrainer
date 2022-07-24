af9c8ca335429e48ee417e1e3f0d106aec4b6b6b
harr1424 <harrington59802@protonmail.com>
2022-07-24 16:13:05 -0600

    Initial Commit

---
+++ PhoneticTrainer/PhoneticTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneticTrainerApp.swift
+//  PhoneticTrainer
+//
+//  Created by user on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct PhoneticTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
