224f741f4ce47a188bd7b602a34a489e88fb160d
harr1424 <harrington59802@protonmail.com>
2022-07-26 19:38:02 -0600

    list based

--- PhoneticTrainer/AlphabetModel.swift
+++ PhoneticTrainer/AlphabetModel.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var allWords = [String]()
+
+var targetWords = [String]()
 
 let LAPD = ["A" : "Adam",
             "B" : "Boy",
--- PhoneticTrainer/ContentView.swift
+++ PhoneticTrainer/ContentView.swift
@@ -7,30 +7,34 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     
-    let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    
-    @StateObject var speech = SpeechRecognizer()
-    @State private var isRecording = false
-    @State private var currLetter = Int.random(in: 0..<26)
+    @State private var currLetter = alphabet[Int.random(in: 0..<26)]
     @State private var showMessage = false
+    @State private var showingScore = false
     @State private var messageTitle = ""
     @State private var messageBody = ""
     @State private var score = 0
-    @State private var buttonTitle = "Begin"
     @State private var mode = ""
+    @State private var scoreTitle = ""
+    @State private var numQuestion = 0
+    @State private var correctAnswer = false
+    @State private var lettersUsed = [String]()
+    @State private var endingTitle = ""
+    @State private var endingBody = ""
+    @State private var showENdingMessage = false
     
-    func buttonPress() {
-        if isRecording {
-            speech.stopTranscribing()
-            buttonTitle = "Resume"
-            isRecording.toggle()
-        } else {
-            speech.transcribe()
-            buttonTitle = "Stop"
-            isRecording.toggle()
+    func startGame() {
+        if let wordListURL = Bundle.main.url(forResource: "sorted_words", withExtension: "txt") {
+            if let sortedWords = try? String(contentsOf: wordListURL) {
+                allWords = sortedWords.components(separatedBy: "\n")
+                targetWords = allWords.filter { $0.starts(with: currLetter) }
+                
+                return
+            }
         }
+        fatalError("Word list could not be loaded")
     }
     
     func chooseMode() {
@@ -47,38 +51,125 @@ struct ContentView: View {
         mode = "LAPD"
     }
     
-    func getLAPD() -> String {
-        return LAPD[alphabet[currLetter]]!
+    func getWords() -> [String] {
+        var shownWords = targetWords[0...3]
+        
+        if mode == "NATO" {
+            shownWords.append(NATO[currLetter]!)
+        }
+        
+        if mode == "LAPD" {
+            shownWords.append(LAPD[currLetter]!)
+        }
+        
+        let uniqueWords = Array(Set(shownWords))
+        
+        return uniqueWords.shuffled()
     }
     
-    func getNATO() -> String {
-        return NATO[alphabet[currLetter]]!
+    func wordChoice(word: String) {
+        if mode == "NATO" {
+            if word == NATO[currLetter] {
+                score += 1
+                scoreTitle = "Correct!"
+                correctAnswer = true
+            } else {
+                scoreTitle = "Try Again"
+            }
+        }
+        
+        if mode == "LAPD" {
+            if word == LAPD[currLetter] {
+                score += 1
+                scoreTitle = "Correct!"
+                correctAnswer = true
+            } else {
+                scoreTitle = "Try Again"
+            }
+        }
+        
+        showingScore = true
+    }
+    
+    func nextQuestion() {
+        lettersUsed.append(currLetter)
+        
+        if correctAnswer == true {
+            withAnimation {
+                currLetter = alphabet[Int.random(in: 0..<26)]
+                while lettersUsed.contains(currLetter) {
+                    currLetter = alphabet[Int.random(in: 0..<26)]
+                }
+                targetWords = allWords.filter { $0.starts(with: currLetter) }
+                numQuestion += 1
+                correctAnswer = false
+            }
+        }
+    }
+    
+    func checkGameEnd() {
+        if numQuestion >= 26 {
+            endingTitle = "Game Over!"
+            endingBody = "You scored \(score / 26)% accuracy."
+            showENdingMessage = true
+        }
+    }
+    
+    func resetGame() {
+        currLetter = alphabet[Int.random(in: 0..<26)]
+        showMessage = false
+        showingScore = false
+        messageTitle = ""
+        messageBody = ""
+        score = 0
+        mode = ""
+        scoreTitle = ""
+        numQuestion = 0
+        correctAnswer = false
+        lettersUsed = [String]()
+        endingTitle = ""
+        endingBody = ""
+        showENdingMessage = false
+        
+        chooseMode()
     }
     
     var body: some View {
         NavigationView {
             ZStack {
                 LinearGradient(gradient: Gradient(stops: [
-                    .init(color: .red, location: 0.3),
-                    .init(color: .white, location: 0.5),
-                    .init(color: .blue, location: 0.7)
+                    .init(color: .red, location: 0.35),
+                    .init(color: .white, location: 0.4),
+                    .init(color: .blue, location: 0.6)
                 ]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
                 
                 VStack {
-                    Text(alphabet[currLetter])
+                    Text(currLetter)
                         .font(.system(size: 140).weight(.bold))
-                    Spacer()
+                        .foregroundColor(.white)
+                    
                     Spacer()
                     
-                    if speech.waiting {
-                        ProgressView()
-                            .scaleEffect(5)
+                    if mode != "" {
+                        // display list of words
+                        VStack {
+                            List(getWords(), id: \.self) { word in
+                                Button {
+                                    wordChoice(word: word)
+                                    checkGameEnd()
+                                } label: {
+                                    Text(word)
+                                        .foregroundStyle(.secondary)
+                                        .font(.title)
+                                }
+                            }
+                            .listRowInsets(EdgeInsets()) // << to zero padding
+                            .background(.regularMaterial)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                        Spacer()
                     }
-                    
-                    Text(speech.transcript)
-                        .font(.largeTitle)
-                    Spacer()
                 }
             }
             .alert(messageTitle, isPresented: $showMessage) {
@@ -87,36 +178,33 @@ struct ContentView: View {
             } message: {
                 Text(messageBody)
             }
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: nextQuestion)
+            }
+            .alert(endingTitle, isPresented: $showENdingMessage) {
+                Button("Play Again", action: resetGame)
+            } message: {
+                Text(endingBody)
+            }
             .toolbar {
-                ToolbarItem(placement: .bottomBar) {
-                    Button {
-                        buttonPress()
-                    } label: {
-                        Text(buttonTitle)
-                            .foregroundColor(.white)
-                            .font(.largeTitle.weight(.bold))
-                    }
-                }
-                ToolbarItem(placement: .principal) {
-                    Image(systemName: isRecording ? "mic" : "mic.slash")
-                        .font(.title)
-                }
                 ToolbarItem() {
                     Text(mode)
                         .font(.largeTitle)
+                        .foregroundColor(.white)
+                    
                 }
                 ToolbarItem(placement: .navigationBarLeading) {
                     Text("Score: \(score)")
                         .font(.largeTitle)
+                        .foregroundColor(.white)
                 }
             }
             .onAppear {
-                speech.reset()
                 chooseMode()
+                startGame()
             }
             .onDisappear {
-                speech.stopTranscribing()
-                isRecording = false
+                
             }
         }
     }
