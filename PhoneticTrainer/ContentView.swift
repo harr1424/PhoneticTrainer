6d84671027aa7d78ca21cb4bb88d1997dfb37b29
harr1424 <harrington59802@protonmail.com>
2022-07-28 18:37:00 -0600

    game logic working 100%

--- PhoneticTrainer/ContentView.swift
+++ PhoneticTrainer/ContentView.swift
@@ -12,18 +12,18 @@ struct ContentView: View {
     
     @State private var currLetter = alphabet[Int.random(in: 0..<26)]
     @State private var showMessage = false
-    @State private var showingScore = false
     @State private var messageTitle = ""
     @State private var messageBody = ""
     @State private var score = 0
     @State private var mode = ""
-    @State private var scoreTitle = ""
     @State private var numQuestion = 0
+    @State private var lettersRemaining = 26
+    @State private var availableLetters = alphabet
     @State private var correctAnswer = false
-    @State private var lettersUsed = [String]()
     @State private var endingTitle = ""
     @State private var endingBody = ""
     @State private var showEndingMessage = false
+    
     
     func startGame() {
         if let wordListURL = Bundle.main.url(forResource: "sorted_words", withExtension: "txt") {
@@ -39,7 +39,7 @@ struct ContentView: View {
     
     func chooseMode() {
         messageTitle = "Select phonetic alphabet"
-        messageBody = "The NATO phonetic alphabet is used by military and paramilitary organizations as well as private entities that frequently enage in telephony (i.e. airline reservations made via phone). The LAPD phonetic alphabet is used primarily by public safety dispatchers in North America."
+        messageBody = "The NATO phonetic alphabet is used by military and paramilitary organizations as well as businesses that frequently enage in telephony (i.e. airline and freight call centers).\n\nThe LAPD phonetic alphabet is used primarily by public safety dispatchers in North America."
         showMessage = true
     }
     
@@ -52,6 +52,8 @@ struct ContentView: View {
     }
     
     func getWords() -> [String] {
+        targetWords = allWords.filter { $0.starts(with: currLetter) }.shuffled()
+        
         var shownWords = targetWords[0...3]
         
         if mode == "NATO" {
@@ -71,63 +73,65 @@ struct ContentView: View {
         if mode == "NATO" {
             if word == NATO[currLetter] {
                 score += 1
-                scoreTitle = "Correct!"
                 correctAnswer = true
             } else {
-                scoreTitle = "Try Again"
+                withAnimation {
+                    correctAnswer = false
+                }
             }
         }
         
         if mode == "LAPD" {
             if word == LAPD[currLetter] {
                 score += 1
-                scoreTitle = "Correct!"
                 correctAnswer = true
             } else {
-                scoreTitle = "Try Again"
+                withAnimation {
+                    correctAnswer = false
+                }
             }
         }
-        
-        showingScore = true
-        checkGameEnd()
+        nextQuestion()
     }
+    
+    
     
     func nextQuestion() {
-        lettersUsed.append(currLetter)
+        numQuestion += 1
+        lettersRemaining -= 1
+        correctAnswer = false
         
-        if correctAnswer == true {
-            withAnimation {
-                currLetter = alphabet[Int.random(in: 0..<26)]
-                while lettersUsed.contains(currLetter) {
-                    currLetter = alphabet[Int.random(in: 0..<26)]
-                }
-                targetWords = allWords.filter { $0.starts(with: currLetter) }
-                numQuestion += 1
-                correctAnswer = false
-            }
+        availableLetters.removeAll(where : {$0 == currLetter})
+        
+        print(lettersRemaining, availableLetters)
+        
+        guard lettersRemaining > 0 else {
+            endGame()
+            
+            return
         }
+        currLetter = availableLetters[Int.random(in: 0..<lettersRemaining)]
+        
+        targetWords = getWords()
     }
     
-    func checkGameEnd() {
-        if numQuestion >= 25 {
-            endingTitle = "Game Over!"
-            endingBody = "You scored \((score / 26) * 100)% accuracy."
-            showEndingMessage = true
-        }
+    func endGame() {
+        endingTitle = "Game Over!"
+        endingBody = String(format: "You scored %.0f%% accuracy", ((Double(score) / 26) * 100))
+        showEndingMessage = true
     }
     
     func resetGame() {
         currLetter = alphabet[Int.random(in: 0..<26)]
         showMessage = false
-        showingScore = false
         messageTitle = ""
         messageBody = ""
         score = 0
         mode = ""
-        scoreTitle = ""
         numQuestion = 0
+        lettersRemaining = 26
+        availableLetters = alphabet
         correctAnswer = false
-        lettersUsed = [String]()
         endingTitle = ""
         endingBody = ""
         showEndingMessage = false
@@ -149,7 +153,7 @@ struct ContentView: View {
                     Text(currLetter)
                         .font(.system(size: 140).weight(.bold))
                         .foregroundColor(.white)
-                    
+                        .animation(.easeIn)
                     Spacer()
                     
                     if mode != "" {
@@ -167,6 +171,7 @@ struct ContentView: View {
                             .listRowInsets(EdgeInsets()) // << to zero padding
                             .background(.regularMaterial)
                             .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .animation(.easeIn)
                         }
                         Spacer()
                     }
@@ -177,9 +182,6 @@ struct ContentView: View {
                 Button("LAPD", action: setLAPD)
             } message: {
                 Text(messageBody)
-            }
-            .alert(scoreTitle, isPresented: $showingScore) {
-                Button("Continue", action: nextQuestion)
             }
             .alert(endingTitle, isPresented: $showEndingMessage) {
                 Button("Play Again", action: resetGame)
@@ -210,8 +212,12 @@ struct ContentView: View {
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+
