81c260f666eafc98b8e46853132df18cd95a7a11
harr1424 <harrington59802@protonmail.com>
2022-07-28 19:00:01 -0600

    added comments

--- PhoneticTrainer/ContentView.swift
+++ PhoneticTrainer/ContentView.swift
@@ -10,21 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // Get a random alphabet character
     @State private var currLetter = alphabet[Int.random(in: 0..<26)]
+    
+    // Display a message upon starting game prompting user to choose a mode
+    @State private var mode = ""
     @State private var showMessage = false
     @State private var messageTitle = ""
     @State private var messageBody = ""
-    @State private var score = 0
-    @State private var mode = ""
-    @State private var numQuestion = 0
-    @State private var lettersRemaining = 26
+    
+    // Store an array of alphabet characters that have not yet been shown to the user
     @State private var availableLetters = alphabet
+    @State private var lettersRemaining = 26
+
+
+    @State private var score = 0
     @State private var correctAnswer = false
+    
+    // Display an ending message to the user showing score and prompting them to play again
     @State private var endingTitle = ""
     @State private var endingBody = ""
     @State private var showEndingMessage = false
     
     
+    /* Called in .onAppear(), loads a wordlist from the app bundle and sets targetWords to contain
+     all words beginning with the current randomly chosen character. */
     func startGame() {
         if let wordListURL = Bundle.main.url(forResource: "sorted_words", withExtension: "txt") {
             if let sortedWords = try? String(contentsOf: wordListURL) {
@@ -37,6 +47,8 @@ struct ContentView: View {
         fatalError("Word list could not be loaded")
     }
     
+    /* Called in .onAppear() and resetGame(), displays a message to the user containing information
+     about the different phonetic alphabets and prompts them to select one. */
     func chooseMode() {
         messageTitle = "Select phonetic alphabet"
         messageBody = "The NATO phonetic alphabet is used by military and paramilitary organizations as well as businesses that frequently enage in telephony (i.e. airline and freight call centers).\n\nThe LAPD phonetic alphabet is used primarily by public safety dispatchers in North America."
@@ -51,10 +63,14 @@ struct ContentView: View {
         mode = "LAPD"
     }
     
+    /* Called in nextQuestion(), each time a new question is loaded, updates targetWords to contain
+     words beginning with the current randomly chosen character. Shuffles this array, adds the correct
+     answer from either the NATO or LAPD alphabet, removes potential duplicates, and shuffles the result
+     so that the correct answer is randomly placed in the list of choices. */
     func getWords() -> [String] {
         targetWords = allWords.filter { $0.starts(with: currLetter) }.shuffled()
         
-        var shownWords = targetWords[0...3]
+        var shownWords = targetWords[0...4]
         
         if mode == "NATO" {
             shownWords.append(NATO[currLetter]!)
@@ -69,6 +85,8 @@ struct ContentView: View {
         return uniqueWords.shuffled()
     }
     
+    /* Called each time a user selects a word, evaluates the chosen word for correctness, updates the
+     running score, and calls nextQuestion() to load the next question. */
     func wordChoice(word: String) {
         if mode == "NATO" {
             if word == NATO[currLetter] {
@@ -94,17 +112,15 @@ struct ContentView: View {
         nextQuestion()
     }
     
-    
-    
+    /* Decrements lettersRemaining and removes the previously shown character from the array of available
+     characters. Provided there are still unusedcharacters remaining, selects a new randomly chosen character to display.
+     If all alphabet characters have been used, endGame() is called. */
     func nextQuestion() {
-        numQuestion += 1
         lettersRemaining -= 1
         correctAnswer = false
         
         availableLetters.removeAll(where : {$0 == currLetter})
-        
-        print(lettersRemaining, availableLetters)
-        
+                
         guard lettersRemaining > 0 else {
             endGame()
             
@@ -115,12 +131,14 @@ struct ContentView: View {
         targetWords = getWords()
     }
     
+    // Displays ending message to user and prompts them to play again
     func endGame() {
         endingTitle = "Game Over!"
         endingBody = String(format: "You scored %.0f%% accuracy", ((Double(score) / 26) * 100))
         showEndingMessage = true
     }
     
+    // Reset the game state to original condition 
     func resetGame() {
         currLetter = alphabet[Int.random(in: 0..<26)]
         showMessage = false
@@ -128,7 +146,6 @@ struct ContentView: View {
         messageBody = ""
         score = 0
         mode = ""
-        numQuestion = 0
         lettersRemaining = 26
         availableLetters = alphabet
         correctAnswer = false
@@ -204,9 +221,6 @@ struct ContentView: View {
             .onAppear {
                 chooseMode()
                 startGame()
-            }
-            .onDisappear {
-                
             }
         }
     }
